6069053ea4ed81047a7271139c19f0fe3358501f
pasanteSystem <pasantesistemas@sercoinfal.com>
2026-03-17 07:57:43 -0400

    Delete createCarnet directory

--- createCarnet/cerrarRegistro.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.botonCerrar').addEventListener('click', function() {
-    window.location.reload();
-        // Accedemos a la página principal (parent) y desactivamos el checkbox
-        window.parent.document.getElementById('ventana').checked = false;
-        window.parent.document.getElementById('ventana2').checked = false;
-
-        
-    });
